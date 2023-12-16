13c2180b4685dfcf8c729f98ad1cd7e88beeb1b6
ahmedMohamed <ahmedaltokey320@gmail.com>
2023-12-16 18:41:22 +0200

    pop

--- components/nav/MainNav.js
+++ components/nav/MainNav.js
@@ -1,13 +1,29 @@
-import React, { Fragment } from "react";
+"use client";
+import React, { Fragment, useState } from "react";
 import classes from "./MainNav.module.css";
 import Link from "next/link";
 import { IoPersonOutline } from "react-icons/io5";
+import { MdCopyAll } from "react-icons/md";
+
 const MainNav = (props) => {
+    const [showComponent, setShowComponent] = useState(false);
+
+    const handleMouseEnter = () => {
+        setShowComponent(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowComponent(false);
+    };
+
+    const handleCopyClick = (text) => {
+        navigator.clipboard.writeText(text);
+        alert("تم النسخ")
+    };
+
     return (
         <Fragment>
-            <nav
-                className={` ${classes.nav}`}
-            >
+            <nav className={` ${classes.nav}`}>
                 <div>
                     <Link href="/" className={classes.logo}>
                         <img
@@ -17,19 +33,51 @@ const MainNav = (props) => {
                         />
                     </Link>
                 </div>
-                <div className={classes.div2}>
-
-                </div>
+                <div className={classes.div2}></div>
                 <div>
-                    <p>
-                        خدمه العملاء
-                    </p>
-                    <p>
-                        +201280626320
-                    </p>
+                    <p>خدمه العملاء</p>
+                    <p>+201280626320</p>
                 </div>
-                <div onClick={props.onShowAside} className="cursor-pointer text-4xl flex justify-center items-center">
+                <div
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    onClick={props.onShowAside}
+                    className="cursor-pointer text-4xl flex justify-center items-center relative"
+                >
                     <IoPersonOutline />
+                    {showComponent && (
+                        <div className={classes.popAccount}>
+                            <ul>
+                                <li>
+                                    <p>كودك الحالي</p>
+                                    <div>
+                                        <p>Ahmed50</p>
+                                        <MdCopyAll
+                                            className="text-3xl"
+                                            onClick={() =>
+                                                handleCopyClick("Ahmed50")
+                                            }
+                                        />
+                                    </div>
+                                </li>
+                                <li>
+                                    <p>لينك الاحاله الخاص بك</p>
+                                    <div>
+                                        <p>https://commacare.net/457165</p>
+                                        <MdCopyAll
+                                            className="text-3xl"
+                                            onClick={() =>
+                                                handleCopyClick(
+                                                    "https://commacare.net/457165"
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                </li>
+                                <li>تسجيل الخروج</li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </nav>
         </Fragment>
